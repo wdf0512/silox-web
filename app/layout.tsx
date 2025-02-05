54f6dc493d5441ebf0406a8741cda777a5b00ecd
De <defangninj@outlook.com>
2025-02-05 17:42:03 +0800

    change the theme.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import "@radix-ui/themes/styles.css";
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 import NavBar from './NavBar';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,9 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Theme>
+        <Theme accentColor="iris" radius="large">
           <NavBar />
           {children}
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
