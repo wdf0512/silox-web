be785d1ca19973b35c28b81c2f2b34324441167f
De <defangninj@outlook.com>
2025-02-07 20:34:03 +0800

    feat: Jump to home page when logo is clicked.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,22 @@
+'use client'
+
 import siloxLogo from '@/public/images/X5.png'
 import { Button, Flex } from '@radix-ui/themes'
 import Image from 'next/image'
 import Link from 'next/link'
 import ServicesDropdown from './components/ServicesDropdown'
+import { useRouter } from 'next/navigation'
 
 const NavBar = () => {
+    const router = useRouter()
+
+    const handleLogoClick = () => {
+        router.push('/')
+    }
+
     return (
         <Flex p="10px 30px" justify="between" align="center">
-            <Image src={siloxLogo} alt="logo" width={100} height={100} />
+            <Image src={siloxLogo} alt="logo" width={100} height={100} className='cursor-pointer' onClick={handleLogoClick} />
 
             <Flex gap="40px" align="center">
                 <ServicesDropdown />
