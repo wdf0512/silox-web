e5c87cbdc8d84b20a33fb30db338f6eace57fe7f
De <defangninj@outlook.com>
2025-02-07 14:26:38 +0800

    done the footer area.

---
+++ app/components/FooterArea.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Box, Flex, Text } from '@radix-ui/themes';
+import React from 'react';
+import Image from 'next/image';
+import FooterBg from '@/public/images/FooterBg.png';
+import WhiteLogo from '@/public/images/WhiteLogo.png';
+import XIcon from '@/public/images/XWhiteLogo.png';
+import LinkedInLogo from '@/public/images/LinkedInLogo.png';
+import YoutubeLogo from '@/public/images/YoutubeLogo.png';
+
+const FooterArea = () => {
+    return (
+        <Box
+            style={{
+                backgroundImage: `url(${FooterBg.src})`,
+                height: '598px',
+                width: '100%',
+                backgroundSize: 'auto ',
+                backgroundPosition: 'center',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                backgroundRepeat: 'no-repeat',
+            }}
+            px={'138px'}
+            className="flex-col md:flex-row items-center text-center md:text-left md: pb-10"
+        >
+            <Flex
+                gap={'8'}
+                direction={'column'}
+                className="w-full md:w-auto items-center md:items-start"
+            >
+                <Image src={WhiteLogo} alt="WhiteLogo" width={200} height={200} />
+                <Text className="text-white text-sm md:text-base">Headquartered in Sydney, Australia</Text>
+            </Flex>
+
+            <Flex
+                gap={'5'}
+                className="mt-6 md:mt-0 flex-wrap justify-center md:justify-start"
+            >
+                <Image className="hover:cursor-pointer w-[50px] md:w-[100px]" src={XIcon} alt="XIcon" width={100} height={100} />
+                <Image className="hover:cursor-pointer w-[50px] md:w-[100px]" src={LinkedInLogo} alt="LinkedInLogo" width={100} height={100} />
+                <Image className="hover:cursor-pointer w-[50px] md:w-[100px]" src={YoutubeLogo} alt="YoutubeLogo" width={100} height={100} />
+            </Flex>
+        </Box>
+    );
+};
+
+export default FooterArea;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import ServicesBlock from './components/ServicesBlock'
 import { Flex } from '@radix-ui/themes'
 import BookConsultationBlock from './components/BookConsultationBlock'
 import ScheduleBlock from './components/ScheduleBlock'
+import FooterArea from './components/FooterArea'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <ServicesBlock />
       <BookConsultationBlock />
       <ScheduleBlock />
+      <FooterArea />
     </Flex>
   )
 }
