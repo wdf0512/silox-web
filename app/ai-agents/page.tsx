8744ba0827dfbc6377bcee1677ce2d1f1651021a
De <defangninj@outlook.com>
2025-02-08 09:32:34 +0800

    done the large title about  AI agent page.

--- app/ai-agents/page.tsx
+++ app/ai-agents/page.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+'use client'
+import { Flex } from '@radix-ui/themes'
+import { motion } from 'motion/react'
+import styles from './styles.module.css'
 
 const AiAgentPage = () => {
     return (
-        <div>AiAgentPage</div>
+        <Flex justify={'center'} align={'center'} py={'8'} height={'500px'}>
+            <motion.h1
+                className={styles['animated-gradient-text']}
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 1 }}
+            >
+                AI Agents & Automation
+            </motion.h1>
+        </Flex>
     )
 }
 
